37dfb8e1d2f210410ad5654635262b4c03a26868
Yogananda <yogananda.chandramouli@necam.com>
2024-05-06 17:16:19 -0700

    Use the subscriber name if provided, or just print key and value.

--- src/main/java/com/websockets/demo/memcache/MessageSubscriber.java
+++ src/main/java/com/websockets/demo/memcache/MessageSubscriber.java
@@ -2,6 +2,7 @@ package com.websockets.demo.memcache;
 
 import com.websockets.demo.websocket.PushJobsWebSocket;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class MessageSubscriber implements MessageListener {
@@ -13,7 +14,12 @@ public class MessageSubscriber implements MessageListener {
 
     @Override
     public void onMessageAdded(String key, String message) {
-        PushJobsWebSocket.broadcast("[" + key + "] " + ": " + message);
+        if(StringUtils.isBlank(name) && !name.equals(key)) {
+            PushJobsWebSocket.broadcast("[" + key + "] " + ": " + message);
+        } else {
+            PushJobsWebSocket.broadcast("[" + name + "] " + key + ": " + message);
+        }
+
     }
 }
 
