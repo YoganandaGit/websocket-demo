635bb996aa958f92ca87ac37e05bd1f4ce850e86
Yogananda <yogananda.chandramouli@necam.com>
2024-05-08 13:03:56 -0700

    Added Kafka integration.

--- src/main/java/com/websockets/demo/kafka/consumer/JobProcessor.java
+++ src/main/java/com/websockets/demo/kafka/consumer/JobProcessor.java
@@ -1,13 +1,11 @@
 package com.websockets.demo.kafka.consumer;
 
-import com.websockets.demo.memcache.MessageCache;
 import com.websockets.demo.model.JobInfo;
+import com.websockets.demo.websocket.PushJobsWebSocket;
 import io.smallrye.reactive.messaging.annotations.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
-
-import java.util.List;
 
 @ApplicationScoped
 public class JobProcessor {
@@ -16,7 +14,6 @@ public class JobProcessor {
     @Outgoing("jobs")
     @Blocking
     public void process(JobInfo jobInfo) {
-        // simulate some hard working task
-         MessageCache.getInstance().addMessage(jobInfo.getJobId(), String.join(",", jobInfo.getPhase(), jobInfo.getProcess(), jobInfo.getState(), jobInfo.getSystemId()));
+        PushJobsWebSocket.broadcast("[" + jobInfo.getJobId() + "] " + ": " + String.join(",", jobInfo.getPhase(), jobInfo.getProcess(), jobInfo.getState(), jobInfo.getSystemId()));
     }
 }
--- src/main/java/com/websockets/demo/model/JobInfo.java
+++ src/main/java/com/websockets/demo/model/JobInfo.java
@@ -1,24 +1,34 @@
 package com.websockets.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class JobInfo {
 
+    @JsonProperty
     private String phase;
 
+    @JsonProperty
     private String process;
 
+    @JsonProperty
     private String state;
 
+    @JsonProperty
     private String systemId;
 
+    @JsonProperty
     private String jobId;
 
 }
